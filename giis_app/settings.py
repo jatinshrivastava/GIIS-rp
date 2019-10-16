8c2454caf050f6ce2e94ea522c9ab6359f4fca56
Jatin Shrivastava <31873847+jatinsrivastava@users.noreply.github.com>
2019-10-16 11:11:34 +0530

    Changes to make the project compatible for running on pythonanywhere.

--- giis_app/settings.py
+++ giis_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'q+8uqb8hds8s$=enfezbo8g93ze(5%b*dmg5l+@--o+qa^0zax'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['jatinshrivastava.pythonanywhere.com']
 
 
 # Application definition
@@ -123,5 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-LOGIN_REDIRECT_URL = "circular_index"
-LOGOUT_REDIRECT_URL = 'circular_index'
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = 'home'
--- giis_app/urls.py
+++ giis_app/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('users/', include('django.contrib.auth.urls')),
-    path("circular_feed/", include("circular_feed.urls")),
+    path("", include("circular_feed.urls")),
     path("conversation/", include("conversation_interface.urls")),
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('home/', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
